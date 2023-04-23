33aab1ab1f6adfe9a3e597fff22b70952b11784a
kkkscar <a.c.t.1928th@gmail.com>
2023-04-23 21:17:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// let a = 10 
+// if (a === 10){
+//     console.log("верно");
+// } else {
+//     console.log("неверно");
+// } 1
+
+// let a = 50
+// let b = 100
+// if (a > b){
+//     console.log("a больше b");
+// } else if (a < b) {
+//     console.log("a меньше b");
+// } 2
+
+// let a = - 2
+// if (a > 0){
+//     console.log("positive");
+// } else if (a == 0){
+//     console.log("равно");
+// } else if (a < 0){
+//     console.log("negative");
+// } 3
+
+// let number = 45 
+// if (number % 2 == 0){
+// console.log("Четное число");
+// } else{
+//     console.log("Нечетное число");
+// } 4
+
+// let a = 10
+// let b = 2 
+// if (a % b == 0){
+//     console.log("Четное число");
+// } else{
+//     console.log("Нечетное число");
+// } 5
+
+// let s = 10
+// console.log(typeof s);
+
+// let s = true
+// if (s){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+// console.log(typeof s);
+
+// let s = "kanykei"
+// console.log(typeof s); 6
+
+// let a = 2
+// if (a > 1 && a < 9){
+//     console.log("верно");
+// } else {
+//     console.log("неверно");
+// } 7
+
+// let a = 3
+// if (a == 3 || 7) {
+//     console.log(a + 7);
+// } else{
+//     console.log(a / 10);
+// } 8
+
+// let a = 1
+// let b = 4
+// if (a == 0 || a > 0 + b > 5 || b == 5){
+//     console.log();
+// } else {
+//     console.log(a - b);
+// } 9
+
+// let a = 9
+// let b = 8
+// if (a > 4 && a < 5){
+  
+// } else if(b > b == 7 && b < 17){
+//     console.log("верно");
+// }else{
+//     console.log("неверно");
+// } 10
+
+// let month = 8
+// if (month = 3 || 4 || 5){
+//     console.log("весна");
+// }else if(month = 6 || 7 || 8){
+//     console.log("лето");
+// }else if(month = 9 || 10 || 11){
+//     console.log("осень");
+// }else{
+//     console.log("зима");
+// } 11
+
+// let day = 12
+// if (day = 1|| 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 10 ){
+//     console.log("первая");
+// }else if(day = 11|| 12 || 13 || 14 || 15 || 16 || 17 || 18 || 19 || 20 ){
+//     console.log("вторая");
+// }else if(day = 21|| 22 || 23 || 24 || 25 || 26 || 27 || 28 || 29 || 30 || 31){
+//     console.log("третья");
+// } 12
+
+// 13. Вам дана переменная a='12345'. Проверьте, что первым символом этой переменной является цифра 1, 2 или 3. Если это так – выведите ‘да’, в противном случае выведите ‘нет’.
+
+
